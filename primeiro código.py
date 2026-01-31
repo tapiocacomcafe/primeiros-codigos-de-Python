405e11797b618aac0efed33f9d2e0cd7c783ac2e
🦇Tapioca com café🦇 <andersonherminio16@gmail.com>
2026-01-30 22:07:49 -0300

    Add files via upload

---
+++ primeiro código.py
@@ -0,0 +1,4 @@
+# Pergunta o nome do usuario
+nome = input('qual o seu nome?')
+# Mostra uma mensagem de boas-vindas de acordo com o valor anterior
+print('boas-vindas','✨' + nome + '✨', 'É um prazer conhecer você!😁')
